194e90c399afaf4ed756b49aad1bb0c62b33489d
Rizkirz <ramadhan.rtio@gmail.com>
2022-02-18 20:43:19 +0700

    lathihan

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
-print("Hello")
-var = 1 + 1
-ver = 2 + 2
-print(var + ver)
+# print("Hello")
+# var = 1 + 1
+# ver = 2 + 2
+# print(var + ver)
+# 0+1 = 1+2 = 3 + 3 = 6 + 4
+numbers = range(1, 5)
+sum = 0
+
+for i in numbers:
+    sum += i
+print ("sum", sum)
